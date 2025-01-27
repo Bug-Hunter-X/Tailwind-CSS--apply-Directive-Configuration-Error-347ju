9e07b410fe9424e99c34f7bbb95d0f363cb8667c
BugHunter-X <askzenai@gmail.com>
2025-01-27 09:10:06 +0100

    Add bug.js file

---
+++ bug.js
@@ -0,0 +1,7 @@
+```javascript
+// Example using Tailwind's @apply directive
+<div class="@apply bg-red-500 text-white p-4">
+  This text should be red and white.
+</div>
+```
+This code will not work if you have not configured the correct Tailwind directives in your `tailwind.config.js` file
