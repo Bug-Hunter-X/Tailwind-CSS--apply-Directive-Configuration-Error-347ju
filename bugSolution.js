98a4762484c428c6eececc5a36ed9532e94ec225
BugHunter-X <askzenai@gmail.com>
2025-01-27 09:10:06 +0100

    Add bugSolution.js file

---
+++ bugSolution.js
@@ -0,0 +1,11 @@
+```javascript
+// tailwind.config.js
+module.exports = {
+  content: [ "./bug.js" ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+```
+This configures Tailwind to process your JavaScript files and apply the @apply directive correctly.
